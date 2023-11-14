930924b731dfbd74446b30341d3b3232e6315501
Digvijay6 <88259876+Digvijay6@users.noreply.github.com>
2023-11-14 16:39:30 +0530

    Update mythread.c

--- mythread.c
+++ mythread.c
@@ -1,52 +1,117 @@
-#ifndef MYTHREAD_H
-#define MYTHREAD_H
-
+#include "MyThread.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 #include "allstdlib.h"
-#include <pthread.h>
 
-#define MAX_THREADS 10
+struct ThreadControlBlock tcb[MAX_THREADS];
+struct statistics stats[MAX_THREADS];
 
-typedef enum {
-    READY,
-    RUNNING,
-    SUSPENDED,
-    SLEEPING,
-    FINISHED
-} ThreadStatus;
+pthread_t masterThread;
 
-struct ThreadControlBlock {
-    pthread_t tid;         // Thread ID
-    int threadID;          // Thread number
-    ThreadStatus status;    // Thread status
-    void *(*function)(void *);  // Thread function
-    void *arg;             // Function argument
-    void *result;          // Thread result
-};
+void initThreadLibrary(void) {
+    masterThread = pthread_self();
+    // Initialize thread library data structures
+    // You may need to add more initialization steps
+}
 
-struct statistics {
-    int id;
-    int runCount;
-    int sleepCount;
-    // Add more fields as needed
-};
+int create(void (*f)(void)) {
+    return createWithArgs((void *(*)(void *))f, NULL);
+}
 
-void initThreadLibrary(void);
-int create(void (*f)(void));
-int createWithArgs(void *(*f)(void *), void *arg);
-int getID(void);
-void dispatch(int signalID);
-void start(void);
-void run(int threadID);
-void suspend(int threadID);
-void resume(int threadID);
-void yield(void);
-void initStatistics(struct statistics *stat, int id);
-void deleteThread(int threadID);
-void sleep1(int sec);
-struct statistics *getStatus(int threadID);
-int createWithArgs(void *(*f)(void *), void *arg);
-void clean(void);
-void JOIN(int threadID);
-void *GetThreadResult(int threadID);
+int createWithArgs(void *(*f)(void *), void *arg) {
+    // Find an available thread control block (tcb) slot
+    int i;
+    for (i = 0; i < MAX_THREADS; i++) {
+        if (tcb[i].status == FINISHED) {
+            tcb[i].status = READY;
+            tcb[i].function = f;
+            tcb[i].arg = arg;
+            tcb[i].result = NULL;
+            pthread_create(&(tcb[i].tid), NULL, f, arg);
+            initStatistics(&stats[i], i);
+            return i;
+        }
+    }
+    return -1; // No available tcb slot
+}
 
-#endif // MYTHREAD_H
+int getID(void) {
+    pthread_t self = pthread_self();
+    for (int i = 0; i < MAX_THREADS; i++) {
+        if (pthread_equal(self, tcb[i].tid)) {
+            return i;
+        }
+    }
+    return -1; // Not found
+}
+
+void dispatch(int signalID) {
+    // Implement your scheduler logic here
+    // This is a simple example, you may want to replace it with a more sophisticated scheduler
+    if (signalID == SIGALRM) {
+        yield();
+    }
+}
+
+void start(void) {
+    // Implement your master thread logic here
+    // This function may need to be extended depending on your requirements
+    signal(SIGALRM, dispatch);
+    alarm(1); // Dispatch every 1 second
+    while (1) {
+        // Your master thread logic
+    }
+}
+
+void run(int threadID) {
+    tcb[threadID].status = RUNNING;
+}
+
+void suspend(int threadID) {
+    tcb[threadID].status = SUSPENDED;
+}
+
+void resume(int threadID) {
+    tcb[threadID].status = READY;
+}
+
+void yield(void) {
+    pthread_kill(tcb[getID()].tid, SIGALRM);
+}
+
+void initStatistics(struct statistics *stat, int id) {
+    stat->id = id;
+    stat->runCount = 0;
+    stat->sleepCount = 0;
+    // Initialize other fields as needed
+}
+
+void deleteThread(int threadID) {
+    pthread_cancel(tcb[threadID].tid);
+    tcb[threadID].status = FINISHED;
+}
+
+void sleep1(int sec) {
+    tcb[getID()].status = SLEEPING;
+    stats[getID()].sleepCount++;
+    sleep(sec);
+    tcb[getID()].status = READY;
+}
+
+struct statistics *getStatus(int threadID) {
+    return &stats[threadID];
+}
+
+void clean(void) {
+    // Implement any necessary cleanup logic
+}
+
+void JOIN(int threadID) {
+    pthread_join(tcb[threadID].tid, NULL);
+}
+
+void *GetThreadResult(int threadID) {
+    return tcb[threadID].result;
+}
